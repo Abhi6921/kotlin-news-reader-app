06a8b5a6143072fc202c61f31f15310a4f5bf4ac
Abhishek <abhishek.narvekar80@gmail.com>
2022-12-07 13:49:39 +0100

    encrypted shared preferences

--- app/src/main/java/nl/narvekar/abhishek/student649744/utils/Session.kt
+++ app/src/main/java/nl/narvekar/abhishek/student649744/utils/Session.kt
@@ -1,20 +1,32 @@
 package nl.narvekar.abhishek.student649744.utils
 
 import android.content.Context
-import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
 import nl.narvekar.abhishek.student649744.R
 import nl.narvekar.abhishek.student649744.utils.Constants.AUTH_TOKEN_KEY
 import nl.narvekar.abhishek.student649744.utils.Constants.IS_LOGGED_IN
 import nl.narvekar.abhishek.student649744.utils.Constants.PASSWORD
 import nl.narvekar.abhishek.student649744.utils.Constants.USERNAME
 
+
 object Session {
 
-    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var sharedPreferences: EncryptedSharedPreferences
 
     fun startSession(context: Context) {
-        sharedPreferences = context.getSharedPreferences(context.getString(R.string.app_name), Context.MODE_PRIVATE)
 
+        val masterKey =  MasterKey.Builder(context)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+         sharedPreferences = EncryptedSharedPreferences.create(
+            context,
+            context.getString(R.string.app_name),
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        ) as EncryptedSharedPreferences
     }
 
     fun getAuthToken() : String {
@@ -37,12 +49,11 @@ object Session {
     }
 
     fun saveUserData(username: String, password: String, authToken: String) {
-        sharedPreferences.edit().apply{
+        sharedPreferences.edit().apply {
             putString(USERNAME, username)
             putString(PASSWORD, password)
             putString(AUTH_TOKEN_KEY, authToken)
             putBoolean(IS_LOGGED_IN, true)
         }.apply()
     }
-
 }
